cf4de08407d71fc780636f47f9858b8597c19a67
Dela Cruz, Clarence Kieth M. <clarencekiethdelacruz@gmail.com>
2025-01-04 02:11:53 +0800

    git ignore test

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const express = require("express");
 //creates an express app
 const app = express();
 
+//Routes
+app.get("/", (request, response) => {
+  response.json({ mssg: "Welcome to the app" });
+});
+
 // listen for request
 app.listen(4000, () => {
-  console.log(`listening on port 4000!!!`);
+  console.log(`listening on port 4000!!`);
 });

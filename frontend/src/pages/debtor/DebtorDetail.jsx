1d8b161a432b6bff183a860a8b19ea064fdcad79
Dela Cruz, Clarence Kieth M. <clarencekiethdelacruz@gmail.com>
2025-01-08 22:30:40 +0800

    added animatiopns

--- frontend/src/pages/debtor/DebtorDetail.jsx
+++ frontend/src/pages/debtor/DebtorDetail.jsx
@@ -157,192 +157,193 @@ function DebtorDetail() {
     }
 
     return (
-        <>
-            <Navigation />
-            <div className={styles.container}>
-                <button onClick={handleReturnToList} className={`${styles.button} ${styles.returnButton}`}>
-                    Return to Debtor List
-                </button>
-                <h1 className={styles.heading}>{debtor.name}</h1>
-                <p>Contact: {debtor.contact}</p>
+<>
+    <Navigation />
+    <div className={styles.container}>
+        <button onClick={handleReturnToList} className={`${styles.button} ${styles.returnButton}`}>
+            Return to Debtor List
+        </button>
+        <h1 className={styles.heading}>{debtor.name}</h1>
+        <p>Contact: {debtor.contact}</p>
 
-                <h2>Debts:</h2>
-                <ul>
-                    {debtor.debts.length === 0 ? (
-                        <p>No debts found.</p>
-                    ) : (
-                        debtor.debts.map((debt, index) => (
-                            <li key={index}>
-                                <p>
-                                    {debt.description}: {debt.amount.toFixed(2)}
-                                    <br />
-                                    (Added on:{" "}
-                                    {new Date(debt.date).toLocaleString("en-US", {
-                                        month: "short",
-                                        day: "2-digit",
-                                        year: "numeric",
-                                        hour12: false,
-                                    })}
-                                    )
-                                </p>
-                            </li>
-                        ))
-                    )}
-                </ul>
+        <h2>Debts:</h2>
+        <ul>
+            {debtor.debts.length === 0 ? (
+                <p>No debts found.</p>
+            ) : (
+                debtor.debts.map((debt, index) => (
+                    <li key={index}>
+                        <p>
+                            {debt.description}: {debt.amount.toFixed(2)}
+                            <br />
+                            (Added on:{" "}
+                            {new Date(debt.date).toLocaleString("en-US", {
+                                month: "short",
+                                day: "2-digit",
+                                year: "numeric",
+                                hour12: false,
+                            })}
+                            )
+                        </p>
+                    </li>
+                ))
+            )}
+        </ul>
 
-                <p>Total Balance: {debtor.totalBalance.toFixed(2)}</p>
+        <p>Total Balance: {debtor.totalBalance.toFixed(2)}</p>
 
-                <button onClick={() => setIsDebtAdding(true)} className={styles.button}>
-                    Add Debt
-                </button>
+        <button onClick={() => setIsDebtAdding(true)} className={styles.button}>
+            Add Debt
+        </button>
 
-                {isDebtAdding && (
-                    <div className={styles.bgContainer}>
-                        <div className={styles.formOverlay}>
-                            <div className={styles.formContainer}>
-                                <h3 className={styles.heading}>Add Debt</h3>
-                                <div className={styles.radioGroup}>
-                                    <label>
-                                        <input
-                                            type="radio"
-                                            value="inventory"
-                                            checked={debtType === "inventory"}
-                                            onChange={() => setDebtType("inventory")}
-                                        />{" "}
-                                        Take Item from Inventory
-                                    </label>
-                                    <label>
-                                        <input
-                                            type="radio"
-                                            value="manual"
-                                            checked={debtType === "manual"}
-                                            onChange={() => setDebtType("manual")}
-                                        />{" "}
-                                        Manually Add Product
-                                    </label>
-                                </div>
-
-                                {debtType === "inventory" && (
-                                    <div>
-                                        <h4>Select Inventory Item</h4>
-                                        <ul>
-                                            {inventoryItems.map((item, index) => (
-                                                <li key={index}>
-                                                    <span>
-                                                        {item.name} - {item.price.toFixed(2)}
-                                                    </span>
-                                                    <input
-                                                        type="number"
-                                                        placeholder="Quantity"
-                                                        min="1"
-                                                        value={quantities[item._id] || ""}
-                                                        onChange={(e) =>
-                                                            setQuantities({
-                                                                ...quantities,
-                                                                [item._id]: Number(e.target.value),
-                                                            })
-                                                        }
-                                                    />
-                                                    <button
-                                                        onClick={() => handleAddInventoryItem(item)}
-                                                        className={styles.button}
-                                                    >
-                                                        Add to Debt
-                                                    </button>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                )}
-
-                                {debtType === "manual" && (
-                                    <div>
-                                        <input
-                                            type="text"
-                                            placeholder="Product Description"
-                                            value={description}
-                                            onChange={(e) => setDescription(e.target.value)}
-                                            className={styles.input}
-                                        />
-                                        <input
-                                            type="number"
-                                            placeholder="Amount"
-                                            value={amount === 0 ? "" : amount}
-                                            onChange={(e) => setAmount(Number(e.target.value))}
-                                            className={styles.input}
-                                        />
-                                    </div>
-                                )}
-
-                                <div className={styles.buttonGroup}>
-                                    <button
-                                        onClick={() => {
-                                            handleAddDebt();
-                                            setDescription("");
-                                            setAmount(0);
-                                        }}
-                                        className={styles.button}
-                                    >
-                                        Add Debt
-                                    </button>
-                                    <button
-                                        onClick={() => {
-                                            setIsDebtAdding(false);
-                                            setDescription("");
-                                            setAmount(0);
-                                        }}
-                                        className={styles.button}
-                                    >
-                                        Close
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
-
-                {isPayingDebt && (
-                    <div className={styles.bgContainer}>
-                        <div className={styles.formOverlay}>
-                            <div className={styles.formContainer}>
-                                <h3 className={styles.heading}>Pay Debt</h3>
+        {isDebtAdding && (
+            <div className={`${styles.bgContainer} ${isDebtAdding ? styles.show : ""}`}>
+                <div className={styles.formOverlay}>
+                    <div className={styles.formContainer}>
+                        <h3 className={styles.heading}>Add Debt</h3>
+                        <div className={styles.radioGroup}>
+                            <label>
                                 <input
-                                    type="number"
-                                    placeholder="Amount Paid"
-                                    value={paymentAmount === 0 ? "" : paymentAmount}
-                                    onChange={(e) => setPaymentAmount(Number(e.target.value))}
+                                    type="radio"
+                                    value="inventory"
+                                    checked={debtType === "inventory"}
+                                    onChange={() => setDebtType("inventory")}
+                                />{" "}
+                                Take Item from Inventory
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="manual"
+                                    checked={debtType === "manual"}
+                                    onChange={() => setDebtType("manual")}
+                                />{" "}
+                                Manually Add Product
+                            </label>
+                        </div>
+
+                        {debtType === "inventory" && (
+                            <div>
+                                <h4>Select Inventory Item</h4>
+                                <ul>
+                                    {inventoryItems.map((item, index) => (
+                                        <li key={index}>
+                                            <span>
+                                                {item.name} - {item.price.toFixed(2)}
+                                            </span>
+                                            <input
+                                                type="number"
+                                                placeholder="Quantity"
+                                                min="1"
+                                                value={quantities[item._id] || ""}
+                                                onChange={(e) =>
+                                                    setQuantities({
+                                                        ...quantities,
+                                                        [item._id]: Number(e.target.value),
+                                                    })
+                                                }
+                                            />
+                                            <button
+                                                onClick={() => handleAddInventoryItem(item)}
+                                                className={styles.button}
+                                            >
+                                                Add to Debt
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+
+                        {debtType === "manual" && (
+                            <div>
+                                <input
+                                    type="text"
+                                    placeholder="Product Description"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
                                     className={styles.input}
                                 />
-                                <div className={styles.buttonGroup}>
-                                    <button
-                                        onClick={() => handlePayment(false)}
-                                        className={styles.button}
-                                    >
-                                        Pay Partial
-                                    </button>
-                                    <button
-                                        onClick={() => handlePayment(true)}
-                                        className={styles.button}
-                                    >
-                                        Pay All
-                                    </button>
-                                    <button
-                                        onClick={() => setIsPayingDebt(false)}
-                                        className={styles.button}
-                                    >
-                                        Close
-                                    </button>
-                                </div>
+                                <input
+                                    type="number"
+                                    placeholder="Amount"
+                                    value={amount === 0 ? "" : amount}
+                                    onChange={(e) => setAmount(Number(e.target.value))}
+                                    className={styles.input}
+                                />
                             </div>
+                        )}
+
+                        <div className={styles.buttonGroup}>
+                            <button
+                                onClick={() => {
+                                    handleAddDebt();
+                                    setDescription("");
+                                    setAmount(0);
+                                }}
+                                className={styles.button}
+                            >
+                                Add Debt
+                            </button>
+                            <button
+                                onClick={() => {
+                                    setIsDebtAdding(false);
+                                    setDescription("");
+                                    setAmount(0);
+                                }}
+                                className={styles.button}
+                            >
+                                Close
+                            </button>
                         </div>
                     </div>
-                )}
-
-                <button onClick={() => setIsPayingDebt(true)} className={styles.button}>
-                    Pay Debt
-                </button>
+                </div>
             </div>
-        </>
+        )}
+
+        {isPayingDebt && (
+            <div className={`${styles.bgContainer} ${isPayingDebt ? styles.show : ""}`}>
+                <div className={styles.formOverlay}>
+                    <div className={styles.formContainer}>
+                        <h3 className={styles.heading}>Pay Debt</h3>
+                        <input
+                            type="number"
+                            placeholder="Amount Paid"
+                            value={paymentAmount === 0 ? "" : paymentAmount}
+                            onChange={(e) => setPaymentAmount(Number(e.target.value))}
+                            className={styles.input}
+                        />
+                        <div className={styles.buttonGroup}>
+                            <button
+                                onClick={() => handlePayment(false)}
+                                className={styles.button}
+                            >
+                                Pay Partial
+                            </button>
+                            <button
+                                onClick={() => handlePayment(true)}
+                                className={styles.button}
+                            >
+                                Pay All
+                            </button>
+                            <button
+                                onClick={() => setIsPayingDebt(false)}
+                                className={styles.button}
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )}
+
+        <button onClick={() => setIsPayingDebt(true)} className={styles.button}>
+            Pay Debt
+        </button>
+    </div>
+</>
+
     );
 }
 

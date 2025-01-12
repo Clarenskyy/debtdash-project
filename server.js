b46dd49c12ecd24fce12b36706714ab574716f2d
Dela Cruz, Clarence Kieth M. <clarencekiethdelacruz@gmail.com>
2025-01-12 11:15:30 +0800

    for frontend commits

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const userRoutes = require("./routes/user");
 // Creates an Express app
 const app = express();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "build")));
+
+// Catch-all handler to serve React's index.html for unmatched routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 // Middleware
 app.use(express.json());
 
